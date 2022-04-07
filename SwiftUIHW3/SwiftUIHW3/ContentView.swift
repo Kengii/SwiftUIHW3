13a8a3bf54073f6e19046f6fff36909da3e4c9d1
Kengii <97613101+Kengii@users.noreply.github.com>
2022-04-07 18:30:20 +0300

    Project Created

---
+++ SwiftUIHW3/SwiftUIHW3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIHW3
+//
+//  Created by Владимир Данилович on 7.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIHW3/SwiftUIHW3/SwiftUIHW3App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHW3App.swift
+//  SwiftUIHW3
+//
+//  Created by Владимир Данилович on 7.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHW3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
